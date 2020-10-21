e356c87c370d94873b04315d47fbb47ae80221b4
Alla Kuzminova <alchik@gmail.com>
2020-10-21 00:38:52 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,11 +3,12 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
+  attr_accessor :email_addresses
 
-  @email_addresses
+
 
   def initialize(email_addresses)
-    @email_addresses = parse(email_addresses)
+    email_addresses = parse(email_addresses)
   end
 
   def parse(email_addresses)
